3a5c142f1bdf959817ccea7663602e1c15ed125f
Shirley Irkal <shirley.irkal@sigma-systems.com>
2019-06-04 16:58:12 +0100

    Add details of code needed to get the figures

--- scraper.py
+++ scraper.py
@@ -5,3 +5,10 @@ def get_subscribers(url):
     html = requests.get(url).content
     soup = BeautifulSoup(html)
     return soup.select('#rawCount')[0].text
+
+# to run the code 
+# get_subscribers('https://socialblade.com/youtube/user/pewdiepie/realtime')
+# and
+# get_subscribers('https://socialblade.com/youtube/user/tseries/realtime')
+
+
